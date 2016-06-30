8caa04422ca0276cf0efe748f535013b61a6ee9c
3nrique0 <3nrique.0@gmail.com>
2016-06-30 16:59:50 +0200

    Removed echos from general script

--- blast_and_extract/5_extract_genomic_many_input_files.sh
+++ blast_and_extract/5_extract_genomic_many_input_files.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+#! /bin/bash
 
 ##	Declare a variable with the Path of all files
-
 FILES=/homedir/ortegaabboud/burst/bioinfo_helpdesk/Yi/redox_genes_arabidopsis_20160614/output/*.filterout
 genome=/NAS/NGS/Hevea/Genome/Reyan7-33-97/Hbgenome.fas
 peptides=/NAS/NGS/Hevea/Genome/Reyan7-33-97/Hbgenome.pep.fas
 upstream=2000
 downstream=2000
-
 
 
 ##	Loop on listed files
@@ -16,13 +14,10 @@ for f in $FILES;
 do
 
 ##	Rename the output file, chancge directory and replace ".fasta" with DBused
-outfile=${f/.filterout/.genomic.fasta};
+outfile=${f/.filterout/.genomic.fastab};
 
 ##	Execute python script
 python 4_extract_genomic_sequences.py -p $peptides -g $genome -o $outfile -b $f  -u $upstream -d $downstream;
 
-##	Echo file, remove when working on the cluster
-# echo "Finding genomic fasta for...  $f";
-# echo "Output file...  $outfile";
 
 done
