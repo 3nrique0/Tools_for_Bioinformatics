def2b04196c7a96e0c664e2c0789f5bae97bbcef
3nrique0 <3nrique.0@gmail.com>
2017-03-20 16:31:02 +0100

    added sample blast output, script has a problem to run with argparse.

--- best_blast_hit/bbh.py
+++ best_blast_hit/bbh.py
@@ -1,11 +1,30 @@
 #! /usr/bin/python3.5
 
 import pandas as pd
+import argparse
 
 
-df = pd.DataFrame({'id' : [1,1,1,2,2,3,3,3,3,4,4,5,6,6,6,7,7],
-                'value'  : ["first","second","second","first",
-                            "second","first","third","fourth",
-                            "fifth","second","fifth","first",
-                            "first","second","third","fourth","fifth"]})
-                            
+dfTest = pd.DataFrame({'id' : [1,1,1,2,2,3,3,3,3,4,4,5,6,6,6,7,7],
+    'value'  : ["first","second","second","first",
+    "second","first","third","fourth",
+    "fifth","second","fifth","first",
+    "first","second","third","fourth","fifth"]})
+
+
+
+def __main__():
+	parser = argparse.ArgumentParser(description='''
+        Get only best blast hit (BBH) output from blast results''')
+
+	parser.add_argument("-f", "--file output", dest="blastHandle",
+        type=str, default=None,
+        help='''Blast output file (blast outfmt 6 only)\n''')
+
+
+	args=parser.parse_args()
+    df = pd.read_csv(args.blastHandle, sep="\t")
+
+
+    print(df)
+
+if __name__ == "__main__": __main__()
