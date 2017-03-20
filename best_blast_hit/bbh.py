7be315303c05271dd7de657be1501ff1c2f86cd1
3nrique0 <3nrique.0@gmail.com>
2017-03-20 15:29:14 +0100

    added sample dataframe

--- best_blast_hit/bbh.py
+++ best_blast_hit/bbh.py
@@ -3,3 +3,9 @@
 import pandas as pd
 
 
+df = pd.DataFrame({'id' : [1,1,1,2,2,3,3,3,3,4,4,5,6,6,6,7,7],
+                'value'  : ["first","second","second","first",
+                            "second","first","third","fourth",
+                            "fifth","second","fifth","first",
+                            "first","second","third","fourth","fifth"]})
+                            
